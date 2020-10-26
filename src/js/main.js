c79a5126bcd2759e9aa914b052016c0bb961e816
Ilya Sovin <levenus8supremus@icloud.com>
2020-10-26 15:47:12 +0300

    some changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,12 @@
-window.onload = function() {
+window.onload = function () {
 
-  $('.footer-block-header').on('click', function() {
-    $(this).next().slideToggle()
-    $(this).css("margin-bottom", "10px")
-  })
+  var mediaQuery = window.matchMedia('(max-width: 1110px)');
+
+  if (mediaQuery.matches) {
+    $('.footer-block-header').on('click', function () {
+      $(this).next().slideToggle()
+    }) 
+   }
 
   $('.project-view').slick({
     dots: true,
@@ -14,19 +17,38 @@ window.onload = function() {
   $('.pac-content').slick({
     draggable: false,
     prevArrow: '    <svg class="gallery-arrow mod-prev">\n' +
-    '      <use xlink:href="/assets/img/sprite.svg#arr-left"></use>\n' +
-    '    </svg>',
-  nextArrow: '    <svg class="gallery-arrow mod-next">\n' +
-    '      <use xlink:href="/assets/img/sprite.svg#arr-right"></use>\n' +
-    '    </svg>',
+      '      <use xlink:href="/assets/img/sprite.svg#arr-left"></use>\n' +
+      '    </svg>',
+    nextArrow: '    <svg class="gallery-arrow mod-next">\n' +
+      '      <use xlink:href="/assets/img/sprite.svg#arr-right"></use>\n' +
+      '    </svg>',
   })
 
-    $('.tech-list-item').on('click', function() {
-      $(this).next().slideToggle()
-    })
+  $('.tech-list-item').on('click', function () {
+    $(this).next().slideToggle()
+  })
 
-    $('.tab-item').on('click', function() {
-      $(this).next().slideToggle()
-    })
+  $('.tab-item').on('click', function () {
+    $(this).next().slideToggle()
+  })
 
-  }
+  $('.popup-with-form').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+    removalDelay: 300, //delay removal by X to allow out-animation
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+
+}
